e3b2c18b30dadc045455ebd9f8fe981a83bf642d
Infinitive Witch <infinitivewitch@disroot.org>
2023-04-15 10:29:00 -0300

    feat: update gh settings
    - extends from .github
    - improves nixago template
    
    Signed-off-by: Infinitive Witch <infinitivewitch@disroot.org>

--- scrolls/_spells/devshellModules/imports/nixago.nix
+++ scrolls/_spells/devshellModules/imports/nixago.nix
@@ -2,17 +2,20 @@
   inputs,
   cell,
 }: let
-  inherit (inputs) std-data-collection;
+  inherit (inputs) std std-data-collection;
 in {
   nixago = with std-data-collection.data.configs; [
     treefmt
     lefthook
     editorconfig
     (conform {data = {inherit (inputs) cells;};})
-    (githubsettings {
+    (std.lib.cfg.githubsettings {
       data = {
+        _extends = ".github";
         repository = {
-          description = "The Forbidden Nix Flakes Doctrine";
+          name = "flakegrimoire";
+          description = cell.lib.flake.description;
+          homepage = "https://github.com/infinitivewitch/flakegrimoire";
           topics = "dotfiles, nix, nixos, nixos-configuration, flake, nix-flake, nixos-dotfiles";
         };
       };
